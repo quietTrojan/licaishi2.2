7bece62e70720fac8826d13720f1eeaaa9e87f9e
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-05-17 18:34:12 +0800

    快速提交

--- dist/js/wodeyinhangka.js
+++ dist/js/wodeyinhangka.js
@@ -1,1 +1,15 @@
-
+$(function(){
+    var maskDiv=$('#maskDiv');
+    var confirmPop=$('#confirmPop');
+    var unBind=$('#unBind');
+    var btnS=confirmPop.find('input[type="button"]');
+    btnS.eq(0).on('click',function(){
+        location.assign($('#unBind').data('skipUrl'));
+    });
+    btnS.eq(1).on('click',function(){
+        maskDiv.add(confirmPop).hide();
+    });
+    unBind.on('click',function(){
+        maskDiv.add(confirmPop).show();
+    });
+});
