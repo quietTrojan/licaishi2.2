e6f961aa5dbd31d87da86cd118075bf95c6f6ac0
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-05-25 15:43:24 +0800

    快速提交

---
+++ dist/js/protocol.js
@@ -0,0 +1,9 @@
+$(function(){
+    var parentWindow=null;
+    if(window.parent){
+        parentWindow=window.parent;
+    }
+    if(parentWindow){
+        parentWindow.find('iframe').remove();
+    }
+});
